008b2043d89a906a8de9926fe806b98cc8d26d70
yoshimoto <motokiti2011@gmail.com>
2023-06-01 23:36:16 +0900

    61 work

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,17 +7,17 @@ export const environment = {
 
   Auth: {
     /** CognitoClientId */
-    clientId: '11uniht8ulp89q519rn0pg95co',
+    clientId: '1nv523knj4pid8jns5ipod905i',
     /** CognitoIdentityPoolId */
-    identityPoolId: 'us-east-1:13365e55-55a6-459d-8614-46f32e6dee71',
+    identityPoolId: 'us-east-1:adff3a5a-96ad-4e1e-96b0-7b3468aecdaa',
     /** CognitoUserPoolId */
-    userPoolId: 'us-east-1_6fKA5NP9y',
+    userPoolId: 'us-east-1_hMnGA9jm1',
     /** Region */
     region: 'us-east-1',
   },
   Buket: {
     /** S3UploadBucketName */
-    bucketName: 'hako-mecha-s3-lhj6iocmf3hu-s3bucket-1dfzfji06xzx7',
+    bucketName: 'hako-mecha-s3-1ex93yihre8l2-s3bucket-kmdf49l190qn',
   },
   EndPoint: {
     apiVersion: '/v1',
@@ -25,17 +25,17 @@ export const environment = {
     apiCheckVersion: '/v1',
     apiAuthVersion: '/v1',
     /** APIエンドポイント */
-    apiEmdPoint: 'https://epvewv5vmg.execute-api.us-east-1.amazonaws.com/dev',
+    apiEmdPoint: 'https://c5pf226ksi.execute-api.us-east-1.amazonaws.com/dev',
     /** GsiAPIエンドポイント */
-    apiEmdPointGsi: 'https://nummfnwk56.execute-api.us-east-1.amazonaws.com/dev',
+    apiEmdPointGsi: ' https://kfl3jkx3y6.execute-api.us-east-1.amazonaws.com/dev',
     /** チェックエンドポイント */
-    apiEmdPointCheck: 'https://5sbd8m9rkf.execute-api.us-east-1.amazonaws.com/dev',
+    apiEmdPointCheck: 'https://n5jd4c194i.execute-api.us-east-1.amazonaws.com/dev',
     /** 単体機能エンドポイント */
-    apiEmdPointUNIQUE : 'https://gyvqi2l0w4.execute-api.us-east-1.amazonaws.com/dev',
+    apiEmdPointUNIQUE : 'https://4esxr3t3t6.execute-api.us-east-1.amazonaws.com/dev',
     /** 伝票機能エンドポイント */
-    apiEmdPointSLIPPROSESS : 'https://9qf1mmsig4.execute-api.us-east-1.amazonaws.com/dev',
+    apiEmdPointSLIPPROSESS : 'https://yyvnn0tfl9.execute-api.us-east-1.amazonaws.com/dev',
     /** 認証エンドポイント */
-    apiEmdAuth : 'https://28uqnuggxh.execute-api.us-east-1.amazonaws.com/dev'
+    apiEmdAuth : ' https://wtj87p282f.execute-api.us-east-1.amazonaws.com/dev'
   }
 };
 
